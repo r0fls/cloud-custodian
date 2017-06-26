f1276e36ea21b8e24daf1a58c7538650156c8a2d
Raphael Deem <raphael.deem@ticketmaster.com>
2017-06-26 13:22:32 -0700

    add more cloudwatch event shortcuts; better logging

--- c7n/cwe.py
+++ c7n/cwe.py
@@ -67,6 +67,14 @@ class CloudWatchEvents(object):
             'ids': 'requestParameters.loadBalancerName',
             'source': 'elasticloadbalancing.amazonaws.com'},
 
+        'ElasticsearchDomain': {
+            'ids': 'requestParameters.domainName',
+            'source': 'es.amazonaws.com'},
+
+        'DynamoDBTable': {
+            'ids': 'requestParameters.tableName',
+            'source': 'dynamodb.amazonaws.com"'},
+
         'RunInstances': {
             'ids': 'responseElements.instancesSet.items[].instanceId',
             'source': 'ec2.amazonaws.com'}}
